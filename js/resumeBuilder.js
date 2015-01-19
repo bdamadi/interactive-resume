ef412f76424cb904eec9962e25f5fd96ff9bbb4e
Babak D <bdamadi@yahoo.com>
2015-01-19 18:24:04 -0500

    Add Images

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -132,3 +132,19 @@ var projects = {
     ]
 };
 
+if (bio.skills.length > 0) {
+
+	$("#header").append(HTMLskillsStart);
+
+	var formattedSkills = HTMLskills.replace("%data%", bio.skills[0]);
+	$("#skills").append(formattedSkills);
+	formattedSkills = HTMLskills.replace("%data%",bio.skills[1]);
+	$("#skills").append(formattedSkills);
+	formattedSkills = HTMLskills.replace("%data%",bio.skills[2]);
+	$("#skills").append(formattedSkills);
+	formattedSkills = HTMLskills.replace("%data%",bio.skills[3]);
+	$("#skills").append(formattedSkills);
+
+
+
+}
